a2e540b9c9536fa26b8c94ba987df5ec31c9fb02
Erik Tuerke <etuerke@googlemail.com>
2016-07-01 09:15:59 +0200

    add generic approach to add options to setup.py

--- pyssemble/python/python_setup_py_creator.py
+++ pyssemble/python/python_setup_py_creator.py
@@ -11,17 +11,31 @@ version="${project.version}",
 author="${project.author}",
 author_email="${project.author_email}",
 url="${project.url}",
-namespace_packages=["pcs"],
-packages=find_packages())
+packages=find_packages(),
+{additional_options}
+)
 '''
 
 
 class PythonSetupPyCreator:
+    FILE_NAME = "setup.py"
+
     def __init__(self, config):
         self.config = config
 
     def create(self, directory):
         setup_py_template = DottedTemplate(SETUP_PY_TEMPLATE)
+        setup_py = setup_py_template.substitute(self.config)
 
-        with open(os.path.join(directory, "setup.py"), "w") as setup_py_pointer:
-            setup_py_pointer.write(setup_py_template.substitute(self.config))
+        additional_options = self._get_additional_options()
+        setup_py = setup_py.format(additional_options=additional_options)
+
+        with open(os.path.join(directory, PythonSetupPyCreator.FILE_NAME), "w") as setup_py_pointer:
+            setup_py_pointer.write(setup_py)
+
+    def _get_additional_options(self):
+        additional_options = ""
+        if "python.namespace_packages" in self.config:
+            additional_options = "namespace_packages=%s" % self.config["python.namespace_packages"]
+
+        return additional_options
